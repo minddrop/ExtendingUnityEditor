54f258e0ae2e7f536617c5ee39a5c6ab8fd6bcc0
minddrop <ryooooooo1@gmail.com>
2019-11-28 12:14:57 +0900

    implemented restart button in menu to restart unityeditor

---
+++ Editor/FileItem.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Diagnostics;
+
+public class FileItem
+{
+  [MenuItem("File/Restart")]
+  static void RestartUnityEditor()
+  {
+    string shPath = Application.dataPath + "/Editor/restart_unity_editor.sh";
+
+    string unityEditorPath = EditorApplication.applicationPath;
+    string args = "-projectPath " + Application.dataPath.Replace("/Assets", "");
+    string command = unityEditorPath + " " + args;
+
+    Process.Start("/bin/bash", $"-c \"{shPath} {command}\"");
+    EditorApplication.Exit(0);
+  }
+}
